ae0d2fdce27aa79995df7c2db82ec39a2d47eec4
Florian Jung <flo@windfis.ch>
2023-08-06 21:12:16 +0000

    dumb forwarder that adds a header

---
+++ proxy.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python3
+from http.server import BaseHTTPRequestHandler,HTTPServer
+import argparse, os, random, sys, requests
+
+from socketserver import ThreadingMixIn
+import threading
+
+def check_auth(path, headers, method):
+    print("check_auth ", path, headers, method)
+    return (True, {'Authorization': 'Bearer secret-token:supersecret'})
+
+class ProxyHTTPRequestHandler(BaseHTTPRequestHandler):
+    protocol_version = 'HTTP/1.0'
+    def do_HEAD(self):
+        self.do_GET(body=False)
+        return
+        
+    def do_GET(self, body=True):
+        try:
+            req_header = self.parse_headers()
+            allow, new_headers = check_auth(self.path, req_header, 'get')
+
+            if allow:
+                url = 'https://{}{}'.format(hostname, self.path)
+                resp = requests.get(url, headers=(req_header | new_headers), verify=False)
+
+                self.send_response(resp.status_code)
+                self.send_resp_headers(resp)
+                msg = resp.text
+                if body:
+                    self.wfile.write(msg.encode(encoding='UTF-8',errors='strict'))
+            else:
+                self.send_error(401, 'Unauthenticated')
+        except:
+            self.send_error(502, 'Internetkurort Bad Gateway')
+
+    def do_DELETE(self, body=True):
+        try:
+            req_header = self.parse_headers()
+            allow, new_headers = check_auth(self.path, req_header, 'delete')
+
+            if allow:
+                url = 'https://{}{}'.format(hostname, self.path)
+                resp = requests.delete(url, headers=(req_header | new_headers), verify=False)
+
+                self.send_response(resp.status_code)
+                self.send_resp_headers(resp)
+                msg = resp.text
+                if body:
+                    self.wfile.write(msg.encode(encoding='UTF-8',errors='strict'))
+            else:
+                self.send_error(401, 'Unauthenticated')
+        except:
+            self.send_error(502, 'Internetkurort Bad Gateway')
+
+    def do_POST(self, body=True):
+        try:
+            req_header = self.parse_headers()
+            allow, new_headers = check_auth(self.path, req_header, 'post')
+
+            if allow:
+                url = 'https://{}{}'.format(hostname, self.path)
+                content_len = int(self.headers.getheader('content-length', 0))
+                post_body = self.rfile.read(content_len)
+                resp = requests.post(url, data=post_body, headers=(req_header | new_headers), verify=False)
+                self.send_response(resp.status_code)
+                self.send_resp_headers(resp)
+                if body:
+                    self.wfile.write(resp.content)
+            else:
+                self.send_error(401, 'Unauthenticated')
+        except:
+            self.send_error(502, 'Internetkurort Bad Gateway')
+
+    def parse_headers(self):
+        req_header = {}
+        for line in self.headers:
+            line_parts = [o.strip() for o in line.split(':', 1)]
+            if len(line_parts) == 2:
+                req_header[line_parts[0]] = line_parts[1]
+        return req_header
+
+    def send_resp_headers(self, resp):
+        respheaders = resp.headers
+        print ('Response Header')
+        for key in respheaders:
+            if key.lower() not in ['content-encoding', 'transfer-encoding', 'content-length']:
+                self.send_header(key, respheaders[key])
+        self.send_header('Content-Length', len(resp.content))
+        self.end_headers()
+
+def parse_args(argv=sys.argv[1:]):
+    parser = argparse.ArgumentParser(description='Proxy HTTP requests')
+    parser.add_argument('--port', dest='port', type=int, default=9999,
+                        help='serve HTTP requests on specified port (default: random)')
+    parser.add_argument('--hostname', dest='hostname', type=str, default='en.wikipedia.org',
+                        help='hostname to be processd (default: en.wikipedia.org)')
+    args = parser.parse_args(argv)
+    return args
+
+class ThreadedHTTPServer(ThreadingMixIn, HTTPServer):
+    """Handle requests in a separate thread."""
+
+def main(argv=sys.argv[1:]):
+    global hostname
+    args = parse_args(argv)
+    hostname = args.hostname
+    print('http server is starting on {} port {}...'.format(args.hostname, args.port))
+    server_address = ('127.0.0.1', args.port)
+    httpd = ThreadedHTTPServer(server_address, ProxyHTTPRequestHandler)
+    print('http server is running as reverse proxy')
+    httpd.serve_forever()
+
+if __name__ == '__main__':
+    main()
